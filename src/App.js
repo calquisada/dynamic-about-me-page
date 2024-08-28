d98d7feb117a3b3e1cf957fd82f1fdb096802e16
calquisada <calquisada@aol.com>
2024-08-28 12:27:57 -0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading.jsx";
+import Hobbies from "./Hobbies.jsx";
+import Contact from "./Contact.jsx";
+import "./style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading name="Sarah Smith" />
+      <Hobbies best="Kayaking" outdoor="Running" indoor="Chess" />
+      <Contact greeting="Contact me" callToAction="Schedule a meeting" />
     </div>
   );
-}
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
